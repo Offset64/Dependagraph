064b3c5cbb3f85f5f684ec3be6e20f37f40d8739
Dan <dan@aredherring.tech>
2022-03-08 22:08:26 +0000

    add initial validation checks

--- main.go
+++ main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+)
+
+type Options struct {
+	URI, User, Password, GithubAPISecret string
+}
+
+var (
+	repository string
+	coalesce   bool
+)
+
+func init() {
+	flag.StringVar(&repository, "repository", "", "The repo to seed the graph with.\nMust be in the form of org/repo (e.g, offset46/Dependagraph)")
+	flag.BoolVar(&coalesce, "coalesce", false, "This enables unlimited crawling mode.\nAfter seeding, grab a leaf node and run again with the leaf as the new seed.")
+	flag.Parse()
+}
+
+func main() {
+	opts := Options{
+		URI:             os.Getenv("NEO4J_URI"),
+		User:            os.Getenv("NEO4J_USR"),
+		Password:        os.Getenv("NEO4J_PWD"),
+		GithubAPISecret: os.Getenv("GITHUB_API_SECRET"),
+	}
+
+	if opts.URI == "" {
+		log.Fatalln("NEO4J_URI not set")
+	}
+
+	if opts.User == "" {
+		log.Fatalln("NEO4J_USR not set")
+	}
+
+	if opts.Password == "" {
+		log.Fatalln("NEO4J_PWD not set")
+	}
+
+	if opts.GithubAPISecret == "" {
+		log.Fatalln("GITHUB_API_SECRET not set")
+	}
+
+	if len(strings.Split(repository, "/")) != 2 {
+		log.Fatalln("incorrect format of repository flag")
+	}
+}
